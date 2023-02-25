85cd4a7a19044c8def109f68f07540269c4a8782
justinjvaldez <justinjvaldez@gmail.com>
2023-02-24 20:47:17 -0700

    test commit

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import Head from 'next/head'
+import styles from '@/styles/Home.module.css'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+        <meta name="JVFIT" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>
+        JV FIT
+        <br></br>
+        nav bar
+        <br></br>
+        Health and Wellness Consulting
+        <br></br>
+        Online Personal Training 
+        <br></br>
+        Personal Training - Online..home 1:1
+        <br></br>
+        Body Work
+        <br></br>
+        Rehab
+        <br></br>
+        client testimonials
+        
+
+
+      </main>
+    </>
+  )
+}
